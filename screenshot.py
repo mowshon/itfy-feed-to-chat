06aca5ebc7a7b41abccf232e9e302e216c4e5585
alexander.shakhmukhametov <alexander.shakhmukhametov@ivao.aero>
2020-04-24 14:26:22 +0300

    take screenshot test2

--- screenshot.py
+++ screenshot.py
@@ -6,6 +6,8 @@ options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
 options.add_argument("--test-type")
 options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome(options=options)
 
 def take_screenshot(link):
